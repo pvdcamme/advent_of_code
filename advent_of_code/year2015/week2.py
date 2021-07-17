acd24ce3737ee981fcaadfb40a028c8cd25bada3
Pieter Vandercammen <pieter.vandercammen@excentis.com>
2021-07-17 23:24:41 +0200

    Improve performance by using a generator. Avoids even creating most of the arrays

--- advent_of_code/year2015/week2.py
+++ advent_of_code/year2015/week2.py
@@ -68,34 +68,33 @@ def solve_day_9_part_ab():
 
 
 def solve_day_10_part_ab():
-    def look_and_say_step(start_list):
+    def look_and_say_step(start):
       result = []
-      prev = start_list[0]
+      prev = next(start)
       cnt = 1
-      for idx in start_list[1:]:
+      for v in start:
         if v == prev:
           cnt += 1
         else:
-          result.append(cnt)
-          result.append(prev)
+          yield cnt
+          yield prev
           prev = v
           cnt = 1
-      result.append(cnt)
-      result.append(prev)
-      return result
+      yield cnt
+      yield prev
 
     puzzle_input = [int(d) for d in "1113122113"]
     
-    updated_v1 = puzzle_input
+    updated_v1 = iter(puzzle_input)
     for _ in range(40):
       updated_v1 = look_and_say_step(updated_v1)
 
-    print("Done with v1")
-    updated_v2 = updated_v1
-    for _ in range(50 -40):
+    updated_v1 = list(updated_v1)
+    updated_v2 = iter(updated_v1)
+    for _ in range(50- 40):
       updated_v2 = look_and_say_step(updated_v2)
  
-    return len(updated_v1), len(updated_v2)
+    return len(list(updated_v1)), len(list(updated_v2))
 
 def solve():
     day8_a, day8_b = solve_day_8_part_ab()
@@ -106,12 +105,9 @@ def solve():
     print(f"Day9a: The shortest route is {day9_a} units")
     print(f"Day9b: The longest route is {day9_b} units")
 
-    import time
-    start = time.time()
     day10_a, day10_b = solve_day_10_part_ab()
     print(f"Day10a: The length grows to {day10_a} after applying 40 times")
     print(f"Day10b: The length grows to {day10_b} after applying 50 times")
-    print(f"Done in {time.time() - start}")
 
 if __name__ == "__main__":
     solve()
