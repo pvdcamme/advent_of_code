61ad8f92786972d62406d5093ba989fd48af191b
Pieter Vandercammen <pieter.vandercammen@excentis.com>
2021-07-17 22:28:42 +0200

    A bit more comments

--- advent_of_code/year2015/__init__.py
+++ advent_of_code/year2015/__init__.py
@@ -5,6 +5,12 @@
   Solved for fun, code quality could without doubt be a lot
   better.
 
+  Each challange stands more or less on its own. To introduce
+  some structure in the code, the solutions are split into 
+  separate weeks. This has a decent balance with not too many
+  nor too long files.
+  (with the main intention to have project stay a fun excercise.)
+
 """
 from .week1 import solve as solve_week_1
 from .week2 import solve as solve_week_2
