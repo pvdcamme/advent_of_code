8a094c4982ffb6656ac746c8866dbdf519fe3728
Pieter Vandercammen <pieter.vandercammen@excentis.com>
2021-07-21 17:24:47 +0200

    Prep for day15

--- advent_of_code/year2015/week3.py
+++ advent_of_code/year2015/week3.py
@@ -4,8 +4,11 @@ def get_filepath(file_name):
     """Returns the full path of the file_name"""
     return pathlib.Path(__file__).parent.joinpath(file_name).resolve()
 
+def solve_day_15_part_ab()
+  return 0,0
 def solve():
-    pass
+  day15_a, day15_b = solve_day_15_part_ab()
+  print(f"Highest scoring cookie reach {day15_a} points")
 
 if __name__ == "__main__":
     solve()
