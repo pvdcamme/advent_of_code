4f3fb4a64584d42b59980fa8a7426e9a3c208f19
Pieter Vandercammen <pieter.vandercammen@excentis.com>
2021-07-21 17:32:32 +0200

    Parse the lines of problem 15

--- advent_of_code/year2015/week3.py
+++ advent_of_code/year2015/week3.py
@@ -1,14 +1,24 @@
 import pathlib
+import re
 
 def get_filepath(file_name):
     """Returns the full path of the file_name"""
     return pathlib.Path(__file__).parent.joinpath(file_name).resolve()
 
-def solve_day_15_part_ab()
+def solve_day_15_part_ab():
+  def parse_line(line):
+    matched = re.match("(\w+): capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)", line)
+    assert matched, line
+
+  with open(get_filepath("day_15.txt"),"r") as f:
+    for line in f:
+      parse_line(line)
+
   return 0,0
+
 def solve():
   day15_a, day15_b = solve_day_15_part_ab()
-  print(f"Highest scoring cookie reach {day15_a} points")
+  print(f"Day15b: Highest scoring cookie reach {day15_a} points")
 
 if __name__ == "__main__":
     solve()
