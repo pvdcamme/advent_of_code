64ccaf2dde38d35117b6367f6a6c1cfe6b3ed844
pvdcamme <pvdcamme@gmail.com>
2024-03-10 21:48:21 +0000

    cleanup: autoformatting

--- year2016/week2.py
+++ year2016/week2.py
@@ -78,13 +78,14 @@ def solve_day_8():
 
     return sum(lcd), as_text(lcd)
 
+
 def solve_day_9():
     """ decompressing the text """
     with open(get_filepath("day_9.txt"), "r") as f:
         text = next(f).strip()
 
     def uncompress(initial_txt, inner):
-      """ Taking the state machine approach. 
+        """ Taking the state machine approach. 
           The inner argumetn is called each time one
           of the inner values are expanded.
 
@@ -98,54 +99,55 @@ def solve_day_9():
           between keeping memory overhead reasonably low
           and increasing efficiency by batching.
       """
-      EMPTY_OUT = iter([])
-      data = ""
+        EMPTY_OUT = iter([])
+        data = ""
 
-      def handle_char(ch, data):
-        if '(' == ch:
-          return EMPTY_OUT, handle_range, ""
-        else:
-          return ch, handle_char, data
+        def handle_char(ch, data):
+            if '(' == ch:
+                return EMPTY_OUT, handle_range, ""
+            else:
+                return ch, handle_char, data
 
-      def handle_range(ch, data):
-        if 'x' == ch:
-          data = (int(data), '')
-          return EMPTY_OUT, handle_repeat, data 
-        else:
-          data += ch
-          return EMPTY_OUT, handle_range, data
+        def handle_range(ch, data):
+            if 'x' == ch:
+                data = (int(data), '')
+                return EMPTY_OUT, handle_repeat, data
+            else:
+                data += ch
+                return EMPTY_OUT, handle_range, data
 
-      def handle_repeat(ch, data):
-        range_cnt, str_repeat = data
-        if ')' == ch:
-          data = (int(range_cnt), int(str_repeat), '')
-          return EMPTY_OUT, handle_collect, data
-        else:
-          data = (range_cnt, str_repeat + ch)
-          return EMPTY_OUT, handle_repeat, data
+        def handle_repeat(ch, data):
+            range_cnt, str_repeat = data
+            if ')' == ch:
+                data = (int(range_cnt), int(str_repeat), '')
+                return EMPTY_OUT, handle_collect, data
+            else:
+                data = (range_cnt, str_repeat + ch)
+                return EMPTY_OUT, handle_repeat, data
 
-      def handle_collect(ch, data):
-        range_cnt, repeat, collected = data
-        collected += ch
-        if range_cnt == 1:
-          final = (inner(collected) for _ in range(repeat))
-          
-          return itertools.chain(*final), handle_char, ''
-        else:
-          data = (range_cnt - 1, repeat, collected)
-          return EMPTY_OUT, handle_collect, data
+        def handle_collect(ch, data):
+            range_cnt, repeat, collected = data
+            collected += ch
+            if range_cnt == 1:
+                final = (inner(collected) for _ in range(repeat))
 
-      current_state = handle_char
-      for ch in initial_txt:
-        out, current_state, data = current_state(ch, data)
-        yield from out
+                return itertools.chain(*final), handle_char, ''
+            else:
+                data = (range_cnt - 1, repeat, collected)
+                return EMPTY_OUT, handle_collect, data
+
+        current_state = handle_char
+        for ch in initial_txt:
+            out, current_state, data = current_state(ch, data)
+            yield from out
 
     def no_expand(vals):
-      return iter(vals)
+        return iter(vals)
 
     cache = {}
+
     def recurse_expand(rr):
-      """ Recursively go deeper if the input has more
+        """ Recursively go deeper if the input has more
 
           Using a cache to efficiently deal with all
           the repeated values.
@@ -153,34 +155,38 @@ def solve_day_9():
           Major constraint is trying avoid letting the 
           memory grow too much out of bounds.
       """
-      if rr in cache:
-        return iter((cache[rr],))
-      elif '(' in rr:
-        if len(cache) > 1024:
-          cache.clear()
-        max_size = 16 * 1024
-        elements = [ch for idx, ch in zip(range(max_size), uncompress(rr, recurse_expand))]
+        if rr in cache:
+            return iter((cache[rr], ))
+        elif '(' in rr:
+            if len(cache) > 1024:
+                cache.clear()
+            max_size = 16 * 1024
+            elements = [
+                ch for idx, ch in zip(range(max_size),
+                                      uncompress(rr, recurse_expand))
+            ]
 
-        if len(elements) == max_size:
-          return uncompress(rr, recurse_expand)
+            if len(elements) == max_size:
+                return uncompress(rr, recurse_expand)
+            else:
+                elements = ''.join(elements)
+                cache[rr] = elements
+                return iter((elements, ))
         else:
-          elements = ''.join(elements)
-          cache[rr] = elements
-          return iter((elements,))
-      else:
-        return iter((rr,))
+            return iter((rr, ))
 
     def part_one(text):
-      full_text = list(uncompress(text, no_expand))
-      return len(full_text)
+        full_text = list(uncompress(text, no_expand))
+        return len(full_text)
 
     def part_two(text):
-      expanded= 0
-      for fragment in uncompress(text, recurse_expand):
-        expanded += len(fragment)
-      return expanded
+        expanded = 0
+        for fragment in uncompress(text, recurse_expand):
+            expanded += len(fragment)
+        return expanded
 
     return part_one(text), part_two(text)
+
 
 def solve_day_10():
     """ Robots distributing chips """
@@ -188,79 +194,78 @@ def solve_day_10():
         instructions = [line.strip() for line in f]
 
     class Bot:
-      """ Values and instructions for the Bot.
+        """ Values and instructions for the Bot.
           Oddly enough we don't need to name.
       """
-      def __init__(self):
-        self.values = []
-        self.outputs = []
+        def __init__(self):
+            self.values = []
+            self.outputs = []
 
-      def append(self, val):
-        # Match the list.append signature.
-        # Makes is polymorphic to add a val to
-        # a Bot or an output
-        self.values.append(val)
+        def append(self, val):
+            # Match the list.append signature.
+            # Makes is polymorphic to add a val to
+            # a Bot or an output
+            self.values.append(val)
 
-      def instruction(self, lowout, highout):
-        self.outputs = [lowout, highout]
+        def instruction(self, lowout, highout):
+            self.outputs = [lowout, highout]
 
-      def __call__(self, name, others, outputs):
-        should_act = len(self.values) == 2
-        if should_act:
-          # an easy switch between output locations
-          places = {"output": outputs, "bot": others}
+        def __call__(self, name, others, outputs):
+            should_act = len(self.values) == 2
+            if should_act:
+                # an easy switch between output locations
+                places = {"output": outputs, "bot": others}
 
-          lower, higher = self.outputs
-          lower_type, lower_idx = lower 
-          higher_type, higher_idx = higher 
+                lower, higher = self.outputs
+                lower_type, lower_idx = lower
+                higher_type, higher_idx = higher
 
-          places[lower_type][lower_idx].append(min(self.values))
-          places[higher_type][higher_idx].append(max(self.values))
-          self.values.clear()
-        return should_act
-      
+                places[lower_type][lower_idx].append(min(self.values))
+                places[higher_type][higher_idx].append(max(self.values))
+                self.values.clear()
+            return should_act
 
-
-      def __str__(self):
-        return f"Bot has {self.values} and {self.outputs}"
+        def __str__(self):
+            return f"Bot has {self.values} and {self.outputs}"
 
     def decode_instruction(line, bots):
-      goes_to = "value (\d+) goes to bot (\d+)"
-      hand_over = "bot (\d+) gives low to (output|bot) (\d+) and high to (output|bot) (\d+)"
-      
-      if res := re.match(goes_to, line):
-        val, bot_idx = res.groups()
-        bots[bot_idx].append(int(val))
-      elif res := re.match(hand_over, line):
-        bot_idx, low_type, low_idx, high_type, high_idx = res.groups() 
-        bots[bot_idx].instruction((low_type, low_idx), (high_type, high_idx))
-        pass
-      else:
-        print(f"Not decoded: {line}")
+        goes_to = "value (\d+) goes to bot (\d+)"
+        hand_over = "bot (\d+) gives low to (output|bot) (\d+) and high to (output|bot) (\d+)"
 
+        if res := re.match(goes_to, line):
+            val, bot_idx = res.groups()
+            bots[bot_idx].append(int(val))
+        elif res := re.match(hand_over, line):
+            bot_idx, low_type, low_idx, high_type, high_idx = res.groups()
+            bots[bot_idx].instruction((low_type, low_idx),
+                                      (high_type, high_idx))
+            pass
+        else:
+            print(f"Not decoded: {line}")
 
     bots = collections.defaultdict(Bot)
-    outputs= collections.defaultdict(list)
+    outputs = collections.defaultdict(list)
     for l in instructions:
-      decode_instruction(l,bots)
+        decode_instruction(l, bots)
 
     acted = True
     responsible = None
     while acted:
-      acted = False
-      for name, b in bots.items():
-        if set([61, 17]) == set(b.values) and not responsible:
-          responsible = name
-        acted = b(name, bots, outputs) or acted
-    
+        acted = False
+        for name, b in bots.items():
+            if set([61, 17]) == set(b.values) and not responsible:
+                responsible = name
+            acted = b(name, bots, outputs) or acted
+
     def multiply(*vals):
-      # For second part
-      result = 1
-      for a_val in vals:
-        result = result * a_val
-      return result
+        # For second part
+        result = 1
+        for a_val in vals:
+            result = result * a_val
+        return result
 
     return responsible, multiply(*(outputs["0"] + outputs["1"] + outputs["2"]))
+
 
 def solve():
     day8_a, day8_b = solve_day_8()
@@ -272,6 +277,5 @@ def solve():
     print(f"Day9a: Recursively decompressing give {day9_b} characters")
 
     day10_a, day10_b = solve_day_10()
-    print(f"Day10a: Bot {day10_a} is responsible for handling Microchips 17 and 61")
+    print(f"Day10a: Bot {day10_a} handles Microchips 17 and 61")
     print(f"Day10b: Multiplied values together are {day10_b}")
-
